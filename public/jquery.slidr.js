860cb98f83b0b823868d8f5f0bae295ecb6949d9
Fred Bergman <fred@fbergman.se>
2011-11-24 13:00:16 +0100

    initial commit

---
+++ public/jquery.slidr.js
@@ -0,0 +1,133 @@
+/* 
+ * slidr
+ * 
+ * Dependencies:
+ *   jquery v ?
+ * 
+ */
+
+;(function($) {
+  
+  $.slidr = function(el, options) {
+    
+    // default settings
+    var defaults = {
+      content       : null,
+      width         : 800,
+      height        : 500,
+      thumb_width   : 75,
+      thumb_height  : 75
+    };
+    
+    // use the plugin var to access the modalw object everywhere
+    var plugin = this;
+    
+    // object to hold the merged default and user-provided options
+    plugin.settings = {};
+    
+    // private
+    // the "constructor" that gets called when object is created
+    var init = function() {
+      
+      // merge default and user-provided options
+      plugin.settings = $.extend({}, defaults, options);
+      
+      // make the collection of target elements available throughout the plugin
+      // by making it a public property
+      plugin.el = el;
+      
+      // store list items
+      plugin.items = plugin.el.children();
+      
+      // set current slide
+      plugin.current_slide = 0;
+      
+      // create elements
+      create_elements();
+      
+      // set element styles
+      set_styles();
+      
+      // attach event handlers
+      attach_event_handlers();
+      
+      // set current thumb
+      currnet_thumb();
+      
+    };
+    
+    // private
+    // create all neccessary elements
+    var create_elements = function() {
+      
+      // create wrapper
+      plugin.el.wrap('<div class="slidr-wrapper" />');
+      
+      // create thumbnails
+      plugin.el.clone().appendTo(plugin.el.parent()).attr('class', 'slidr-thumbs');
+      
+      // store thumbs
+      plugin.thumbs = $('.slidr-thumbs');
+      
+      // store thumbs children
+      plugin.thumb_items = plugin.thumbs.children();
+      
+      // remove cloned id
+      plugin.thumbs.removeAttr('id');
+      
+    };
+    
+    // private
+    // set styles
+    var set_styles = function() {
+      
+      // set image container style
+      plugin.el.css({
+        position  : 'relative',
+        width     : plugin.settings.width+'px',
+        height    : plugin.settings.height+'px',
+        overflow  : 'hidden'
+      });
+      
+      // set list item styles
+      plugin.el.children().css({
+        position  : 'absolute',
+        top       : 0
+      }).each(function(i) {
+        
+        // set z-index
+        // start z-index value from total length
+        $(this).css('zIndex', plugin.items.length-i);
+        
+      });
+      
+      // set thumb list elements and its children to settings width and height
+      plugin.thumb_items.add(plugin.thumb_items.children()).css({
+        width   : plugin.settings.thumb_width+'px',
+        height  : plugin.settings.thumb_height+'px'
+      });
+      
+    };
+    
+    // private
+    // attack event handlers
+    var attach_event_handlers = function() {
+      
+      plugin.el.click(function() {
+        plugin.current_slide++;
+      });
+      
+    };
+    
+    // private
+    // set current thumbnail
+    var currnet_thumb = function() {
+      plugin.thumb_items.removeClass('current').eq(plugin.current_slide).addClass('current');
+    };
+    
+    // call the "constructor"
+    init();
+    
+  };
+  
+})(jQuery);
---
+++ spec/javascripts/slidr_spec.coffee
@@ -0,0 +1,118 @@
+# require scripts
+require '/jquery-1.7.1.js'
+require '/jquery.slidr.js'
+
+# settings
+describe 'settings', ->
+  
+  # define var in scope
+  slidr =
+  
+  beforeEach ->
+    slidr = new $.slidr $('#example1'), { width: 500, height: 300, thumb_width: 100, thumb_height: 100 }
+  
+  it 'settings width', ->
+    expect(slidr.settings.width).toEqual(500)
+  
+  it 'settings height', ->
+    expect(slidr.settings.height).toEqual(300)
+    
+  it 'settings thumb width', ->
+    expect(slidr.settings.thumb_width).toEqual(100)
+  
+  it 'settings thumb height', ->
+    expect(slidr.settings.thumb_height).toEqual(100)
+
+# elements
+describe 'elements', ->
+  
+  template 'markup.html'
+  
+  # define var in scope
+  slidr =
+  
+  beforeEach ->
+    slidr = new $.slidr $('#example1'), { thumb_width: 100, thumb_height: 100 }
+  
+  it 'create wrapper', ->
+    expect(slidr.el.parent().attr 'class').toEqual('slidr-wrapper')
+  
+  it 'create thumbs', ->
+    expect(slidr.el.siblings('.slidr-thumbs').length).toEqual(1)
+  
+  it 'thumb width', ->
+    expect(slidr.thumbs.children().width()).toEqual(slidr.settings.thumb_width)
+  
+  it 'thumb height', ->
+    expect(slidr.thumbs.children().height()).toEqual(slidr.settings.thumb_height)
+  
+  it 'current thumb', ->
+    expect(slidr.thumbs.children().eq(0).attr 'class').toEqual('current');
+
+# check styles
+describe 'check styles', ->
+  
+  template 'markup.html'
+  
+  # define var in scope
+  slidr =
+  
+  beforeEach ->
+    slidr = new $.slidr $('#example1'), { width: 500, height: 300 }
+  
+  it 'check slidr width', ->
+    expect(slidr.el.width()).toEqual(500)
+  
+  it 'check slidr height', ->
+    expect(slidr.el.height()).toEqual(300)
+  
+  it 'check overflow', ->
+    expect(slidr.el.css('overflow')).toEqual('hidden')
+  
+  it 'check position', ->
+    expect(slidr.el.css('position')).toEqual('relative')
+  
+  it 'check list item position', ->
+    expect(slidr.items.css('top')).toEqual('0px')
+  
+  it 'check z-index', ->
+    expect(slidr.items.eq(0).css('zIndex')).toEqual(slidr.items.length.toString())
+
+# variables
+describe 'check variables', ->
+  
+  template 'markup.html'
+  
+  # define var in scope
+  slidr =
+  
+  beforeEach ->
+    slidr = new $.slidr $('#example1'), { width: 500, height: 300 }
+  
+  it 'current slide', ->
+    expect(slidr.current_slide).toEqual(0)
+  
+  it 'items', ->
+    expect(slidr.items).toBeDefined()
+  
+  it 'thumb items', ->
+    expect(slidr.thumb_items).toBeDefined();
+
+# event handlers
+describe 'event handlers', ->
+  
+  template 'markup.html'
+  
+  # define var in scope
+  slidr =
+  
+  beforeEach ->
+    slidr = new $.slidr $('#example1'), { width: 500, height: 300 }
+  
+  it 'image click', ->
+    slidr.el.click()
+    expect(slidr.current_slide).toEqual(1);
+  
+  #it 'new slide', ->
+  #  slidr.el.click()
+  #  expect(slidr.items.eq(0).css('zIndex')).toEqual(slidr.el.children().length);
