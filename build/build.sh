050421329b79298fd9838664c7d45f517ea0adce
Fred Bergman <fred@fbergman.se>
2011-11-25 09:05:59 +0100

    fixed build script to match new structure

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # minify slidr
-java -jar /usr/local/bin/compiler.jar --js ../public/jquery.slidr.js ../public/jquery.slidr.thumbnails.js --js_output_file ../public/jquery.slidr.min.js
+java -jar /usr/local/bin/compiler.jar --js ../jquery.slidr.js ../jquery.slidr.thumbnails.js --js_output_file ../jquery.slidr.min.js
 
 echo "All done!"
