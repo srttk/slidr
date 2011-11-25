e46e50cdde12158b356937f135ec484822a8fde2
Joel Larsson <bolmaster2@gmail.com>
2011-11-25 10:17:53 +0100

    add transitions skeleton

---
+++ jquery.slidr.transitions.js
@@ -0,0 +1,13 @@
+// Transitions for the slider
+$.slidr_transitions = function(plugin, options) {
+  
+};
+
+// Before transition
+$.slidr_transitions.prototype.before = function() {
+  
+}
+// After transition
+$.slidr_transitions.prototype.after = function() {
+  
+}
