5f9bc6ee71182bb40d68ec9061584cb0d5cf0583
Joel Larsson <bolmaster2@gmail.com>
2011-11-25 14:25:36 +0100

    update build script to be able to run it from anywhere

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# This script path 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+JS_DIR=$SCRIPT_DIR/..
+OUTPUT_DIR=$SCRIPT_DIR/..
+
+
 # minify slidr
-java -jar /usr/local/bin/compiler.jar --js ../jquery.slidr.js ../jquery.slidr.thumbnails.js --js_output_file ../jquery.slidr.min.js
+java -jar /usr/local/bin/compiler.jar --js $JS_DIR/jquery.slidr.js $JS_DIR/jquery.slidr.thumbnails.js --js_output_file $OUTPUT_DIR/jquery.slidr.min.js
 
 echo "All done!"
