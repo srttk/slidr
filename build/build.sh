51f100ad6abeab2b264880cda35527c7a0327d30
Joel Larsson <bolmaster2@gmail.com>
2011-11-25 15:40:47 +0100

    add transitions file to minifier in build

--- build/build.sh
+++ build/build.sh
@@ -9,6 +9,6 @@ OUTPUT_DIR=$SCRIPT_DIR/..
 
 
 # minify slidr
-java -jar /usr/local/bin/compiler.jar --js $JS_DIR/jquery.slidr.js $JS_DIR/jquery.slidr.thumbnails.js --js_output_file $OUTPUT_DIR/jquery.slidr.min.js
+java -jar /usr/local/bin/compiler.jar --js $JS_DIR/jquery.slidr.js $JS_DIR/jquery.slidr.thumbnails.js $JS_DIR/jquery.slidr.transitions.js --js_output_file $OUTPUT_DIR/jquery.slidr.min.js
 
 echo "All done!"
